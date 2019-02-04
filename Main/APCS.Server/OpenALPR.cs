c24099bd30d0803d415e21e58ae082871e21cd57
MickMelon <michael.mcmillan@outlook.com>
2019-02-04 16:08:21 +0000

    Move OpenALPR stuff to own class

--- Main/APCS.Server/OpenALPR.cs
+++ Main/APCS.Server/OpenALPR.cs
@@ -7,20 +7,46 @@ using System.Threading.Tasks;
 
 namespace APCS.Server
 {
-    public class OpenALPR
+    /// <summary>
+    /// Contains all the functions for working with OpenALPR.
+    /// </summary>
+    public static class OpenALPR
     {
+        /// <summary>
+        /// The private key used for OpenALPR Cloud API.
+        /// </summary>
         private const string SECRET_KEY = "sk_b814e91e23aaf4bfc8403ff3";
+
+        /// <summary>
+        /// The HttpClient object used to interact with OpenALPR Cloud API.
+        /// </summary>
         private static readonly HttpClient _httpClient = new HttpClient();
 
-        public static async Task<string> ProcessImage(string imagePath)
+        /// <summary>
+        /// Reads a registration number from the specified image.
+        /// </summary>
+        /// <param name="imagePath">The path to the vehicle image.</param>
+        /// <returns>A new vehicle object with required data.</returns>
+        public static async Task<Vehicle> ReadReg(string imagePath)
         {
-            byte[] bytes = File.ReadAllBytes(imagePath);
-            string imageBase64 = Convert.ToBase64String(bytes);
+            string response = await ProcessImage(imagePath);
+            return new Vehicle(response);
+        }
 
-            var content = new StringContent(imageBase64);
-            var response = await _httpClient.PostAsync(
-                "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=" + SECRET_KEY,
-                content)
+        /// <summary>
+        /// Sends the vehicle image to OpenALPR gets the result.
+        /// </summary>
+        /// <param name="imagePath">The path to the vehicle image.</param>
+        /// <returns>The result in JSON format.</returns>
+        private static async Task<string> ProcessImage(string imagePath)
+        {
+            Byte[] bytes = File.ReadAllBytes(imagePath);
+            string imagebase64 = Convert.ToBase64String(bytes);
+
+            var content = new StringContent(imagebase64);
+
+            var response = await _httpClient
+                .PostAsync("https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=gb&secret_key=" + SECRET_KEY, content)
                 .ConfigureAwait(false);
 
             var buffer = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
--- Main/APCS.Server/Program.cs
+++ Main/APCS.Server/Program.cs
@@ -8,38 +8,17 @@ using Newtonsoft.Json;
 
 namespace APCS.Server
 {
+    /// <summary>
+    /// The class that starts the program up.
+    /// </summary>
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
-
-        public static async Task<Vehicle> ReadReg(string imagePath)
-        {
-            string response = await ProcessImage(imagePath);
-            var vehicle = new Vehicle(response);
-            return vehicle;
-        }
-
-        public static async Task<string> ProcessImage(string image_path)
-        {
-            string SECRET_KEY = "sk_b814e91e23aaf4bfc8403ff3";
-
-            Byte[] bytes = File.ReadAllBytes(image_path);
-            string imagebase64 = Convert.ToBase64String(bytes);
-
-            var content = new StringContent(imagebase64);
-
-            var response = await client.PostAsync("https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=gb&secret_key=" + SECRET_KEY, content).ConfigureAwait(false);
-
-            var buffer = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
-            var byteArray = buffer.ToArray();
-            var responseString = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
-
-            return responseString;
-        }
-
+        /// <summary>
+        /// The method that starts the program up.
+        /// </summary>
         static void Main(string[] args)
         {
-            Task<Vehicle> recognizeTask = Task.Run(() => ReadReg(@"car.jpg"));
+            Task<Vehicle> recognizeTask = Task.Run(() => OpenALPR.ReadReg(@"car.jpg"));
             recognizeTask.Wait();
             var vehicle = recognizeTask.Result;
 
