a9162fdb4ce549e5d3db63eab7a99f03f38c80d0
MickMelon <michael.mcmillan@outlook.com>
2019-04-09 16:14:22 +0100

    Add JS validation for updating prices

--- Web/app/Views/Admin/permits.php
+++ Web/app/Views/Admin/permits.php
@@ -1,18 +1,18 @@
 <h4>Hourly Rate</h4>
-<form action="index.php?controller=admin&action=updatehourlyrate" method="post">
+<form action="index.php?controller=admin&action=updatehourlyrate" method="post" onsubmit="return checkHourlyRateForm()">
     <div class="form-group">
         <label for="first">Hourly Rate:</label>
-        <input type="price" name="price" class="form-control" id="price" value="<?= $hourlyRate ?>" minlength="0.00" maxlength="1000.00" step="0.01" required>
+        <input type="price" name="price" class="form-control" id="hourlyPrice" value="<?= $hourlyRate ?>" minlength="0.00" maxlength="100.00" step="0.01" required>
         <small id="price" class="form-text text-muted">Caution: This changes the monthly parking permit price.</small>
     </div>
     <button type="submit" class="btn btn-outline-danger">Update Hourly Rate!</button>
 </form>
 <br />
 <h4>Permits</h4>
-<form action="index.php?controller=admin&action=updatepermitprice" method="post">
+<form action="index.php?controller=admin&action=updatepermitprice" method="post" onsubmit="return checkPermitPriceForm()">
     <div class="form-group">
         <label for="first">Permit Price:</label>
-        <input type="price" name="price" class="form-control" id="price" value="<?= $permitPrice ?>" minlength="0.00" maxlength="1000.00" step="0.01" required>
+        <input type="price" name="price" class="form-control" id="permitPrice" value="<?= $permitPrice ?>" minlength="0.00" maxlength="100.00" step="0.01" required>
         <small id="price" class="form-text text-muted">Caution: This changes the monthly parking permit price.</small>
     </div>
     <button type="submit" class="btn btn-outline-danger">Update Permit Price!</button>
@@ -24,3 +24,29 @@
         <small id="rap" class="form-text text-muted">DANGER: THIS BUTTON REMOVES ALL PARKING PERMITS FOR THIS CURRENT MONTH.</small>
     </div>
 </form>
+
+<script>
+function checkHourlyRateForm() {
+    var price = document.getElementById("hourlyPrice").value;
+
+    if (price > 100 || price < 0)
+    {
+        alert("Hourly price cannot be less than 0 or more than 100.");
+        return false;
+    }
+
+    return true;
+}
+
+function checkPermitPriceForm() {
+    var price = document.getElementById("permitPrice").value;
+
+    if (price > 100 || price < 0)
+    {
+        alert("Permit price cannot be less than 0 or more than 100.");
+        return false;
+    }
+
+    return true;
+}
+</script>
