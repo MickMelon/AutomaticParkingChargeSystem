19f327ef477d8c920b8d4e5b721aaeab645f30da
MickMelon <michael.mcmillan@outlook.com>
2019-04-11 17:06:40 +0100

    Update client

--- Client/client.py
+++ Client/client.py
@@ -3,8 +3,8 @@ import requests
 URL = "https://mayar.abertay.ac.uk/~cmp311gc1801/index.php?controller=api&action=check"
 CARPARKID = 1 # ID of the carpark this client belongs to
 
-def check(type, carparkid, reg):
-    params = {'type':type, 'carparkid':carparkid, 'reg':reg}
+def check(carparkid, reg):
+    params = {'carparkid':carparkid, 'reg':reg}
     result = requests.get(url = URL, params = params)
     data = result.json()
 
@@ -22,21 +22,17 @@ def handle_error(error_message):
     print('Some error happened (' + error_message + ')')
     # Maybe some better error handling here
 
-# TODO:
-    # Detect vehicle
-    # Take picture
-    # Put picture through OpenALPR
-    # Set reg variable from result
-    # Check if reg is in car park so we know what type to specify
-    # Call the function below with result
+#
+# CONSUMING THE API EXAMPLE
+#
 
 # Call to the check function
-message = check('exit', CARPARKID, 'sa07enw')
+#message = check(CARPARKID, 'sa07enw')
 
 # Check the result
-if message == 'ENTRY_SUCCESS':
-    handle_entry()
-elif message == 'EXIT_SUCCESS':
-    handle_exit()
-else:
-    handle_error(message)
+#if message == 'ENTRY_SUCCESS':
+#    handle_entry()
+#elif message == 'EXIT_SUCCESS':
+#    handle_exit()
+#else:
+#    handle_error(message)
